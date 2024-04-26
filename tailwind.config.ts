0c8594b1ce4a969ad4521dd6c4be5df4bfa2d646
Jongbum <amjong2@gmail.com>
2024-04-26 23:07:05 +0900

    shadcn installation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,55 +1,80 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss"
 
-const { searchBgSrc } = require('./src/app/data/image/encodedImage');
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    './src/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    screens: {
-      ...defaultTheme.screens,
-      sm: { max: '480px' },
-      // => @media (max-width: 480px) { ... }
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      fontSize: {
-        '3xl': '48px',
-        '2xl': '36px',
-        xl: '24px',
-        lg: '16px',
-        md: '12px',
-      },
       colors: {
-        y1: '#FFC700',
-        y2: '#FFD233',
-        y4: '#FFE380',
-        y5: '#FFF4CC',
-        r1: '#D11B00',
-        r2: '#D74652',
-        r3: '#E88D80',
-        r4: '#F6D1CC',
-        n0: '#1A202C',
-        n1: '#282C47',
-        n2: '#53566C',
-        black: '#1E1E1E',
-        white: '#FFFFFF',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      fontFamily: {
-        regular: ['var(--font-NexonGothicRegular)'],
-        bold: ['var(--font-NexonGothicBold)'],
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-      backgroundImage: {
-        custom: `url(${searchBgSrc})`,
-        fixed: 'fixed',
-        cover: 'cover',
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
 
-export default config;
+export default config
