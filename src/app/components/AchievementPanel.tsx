ae2c81bc14834afadb389bf1f3b7e016e486449a
Jongbum <amjong2@gmail.com>
2024-05-14 00:10:53 +0900

    AchievementPanel.tsx: use MasterCard Component

--- src/app/components/AchievementPanel.tsx
+++ src/app/components/AchievementPanel.tsx
@@ -1,3 +1,4 @@
+import MasterCard from '../ui/MasterCard';
 import PeriodSelectPanel from './PeriodSelectPanel';
 
 export default function AchievementPanel() {
@@ -8,6 +9,30 @@ export default function AchievementPanel() {
         <br />* 2023년 12월 27일 이후의 데이터만 조회 가능합니다.
       </div>
       <PeriodSelectPanel />
+      <div className='grid grid-cols-2 gap-8 px-20 my-20'>
+        <MasterCard className='col-span-2'>
+          <span className='font-bold text-white'>
+            총 사용 메소량/할인으로 아낀 메소량
+          </span>
+        </MasterCard>
+        <MasterCard>
+          <span className='font-regular text-white text-sm'>15성 이상</span>
+          <span className='font-bold text-white'>연속 최다 성공 횟수</span>
+        </MasterCard>
+        <MasterCard>
+          <span className='font-regular text-white text-sm'>평균 스타캐치</span>
+          <span className='font-bold text-white'>성공률</span>
+        </MasterCard>
+        <MasterCard>
+          <span className='font-regular text-white text-sm'>15성 이상</span>
+          <span className='font-bold text-white'>연속 최다 실패 횟수</span>
+        </MasterCard>
+        <MasterCard>
+          <span className='font-bold text-white'>
+            총 사용 메소량/할인으로 아낀 메소량
+          </span>
+        </MasterCard>
+      </div>
     </div>
   );
 }
